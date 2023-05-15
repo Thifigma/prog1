86251695b529b330dd654cea1325a88417343f9f
thifigma <thiago.figueiredo@ufpr.br>
2023-05-14 22:16:36 -0300

    pilha, refazer alguns testes na destroi da 6

--- tp3/libpilha.c
+++ tp3/libpilha.c
@@ -7,9 +7,8 @@ pilha_t *pilha_cria ()
     pilha_t *p;
 
     p = malloc (sizeof(pilha_t));
-    if (!p){
+    if (!p)
         return NULL;
-    }
     
     p->topo = NULL;
     p->tamanho = 0;
@@ -19,24 +18,70 @@ pilha_t *pilha_cria ()
 
 void pilha_destroi (pilha_t **pilha)
 {
-   
+    nodo_t *aux;
+
+    while ( (*pilha)->topo != NULL) {
+        aux = (*pilha)->topo;
+        (*pilha)->topo = (*pilha)->topo->prox;
+        free (aux);
+    }
+
+    free (*pilha);
+    *pilha = NULL;
 }
 
 int push (pilha_t *pilha, int dado)
 {
-    struct nodo *aux;
+    nodo_t *aux;
+    aux = malloc (sizeof(nodo_t));
+    if (!aux)
+        return 0;
 
     aux->dado = dado;
     aux->prox = pilha->topo;
+    pilha->tamanho++;
     pilha->topo = aux;
     if (!pilha->topo)
         return 1;
     else
-        return 0;
-    
+        return 0;   
 }
 
 int pop (pilha_t *pilha, int *dado)
 {
+    nodo_t *aux;
 
+    if (!pilha_vazia(pilha)){
+        aux = pilha->topo->prox;
+        *dado = pilha->topo->dado;
+        free (pilha->topo);
+        pilha->tamanho--;
+        pilha->topo = aux;
+        return 1;
+    }
+
+    return 0;
+}
+
+int pilha_topo (pilha_t *pilha, int *dado)
+{
+    if (!pilha->topo){
+        return 0;
+    }
+
+    *dado = pilha->topo->dado;
+    
+    return 1;
+}
+
+int pilha_vazia (pilha_t *pilha)
+{
+    if (!pilha->topo)
+        return 1;
+    return 0;
+}
+
+int pilha_tamanho (pilha_t *pilha)
+{
+    return pilha->tamanho;
 }

7d35fc4123075ceb101f418ae7dca1974165e4ed
Thiago <thiago.figueiredo@ufpr.br>
2023-05-18 16:33:51 -0300

    refazer dequeue enqueue, segfault em alguma delas.

--- tp4/libfila.c
+++ tp4/libfila.c
@@ -1,12 +1,14 @@
-#include <stddef.h>
+/* #include <stddef.h>  */
+
+#include <stdio.h>
 #include <stdlib.h>
 #include "libfila.h"
 
-fila_t *fila_cria ()
+fila_t *fila_cria()
 {
     fila_t *fila;
 
-    fila = malloc (sizeof(fila_t));
+    fila = malloc(sizeof(fila_t));
     if (!fila)
         return NULL;
 
@@ -17,61 +19,77 @@ fila_t *fila_cria ()
     return fila;
 }
 
-void fila_destroi (fila_t **fila)
+void fila_destroi(fila_t **fila)
 {
     nodo_t *aux;
 
-    while ((*fila)->cabeca != NULL){
-        aux = (*fila)->cabeca; /*Salva a cabeca*/
+    while ((*fila)->cabeca != NULL)
+    {
+        aux = (*fila)->cabeca;                   /*Salva a cabeca*/
         (*fila)->cabeca = (*fila)->cabeca->prox; /*vai em direcao a cauda*/
-        free (aux);
+        free(aux);
     }
 
-    free ((*fila));
+    free((*fila));
     *fila = NULL;
 }
 
-int enqueue (fila_t *fila, int dado)
+int enqueue(fila_t *fila, int dado)
 {
     nodo_t *aux;
-    aux = malloc (sizeof(nodo_t));
+    aux = malloc(sizeof(nodo_t));
     if (!aux)
         return 0;
-    
+
     aux->dado = dado;
-    aux->prox = fila->cauda;
-    fila->cauda = aux;
+    aux->prox = NULL;
+
+    if (fila_vazia(fila))
+    {
+        fila->cabeca = aux;
+        fila->cauda = aux;
+        return 1;
+    }
+
+    fila->cauda->prox = aux;
     fila->tamanho++;
 
     return 1;
 }
 
-int dequeue (fila_t *fila, int *dado)
+int dequeue(fila_t *fila, int *dado)
 {
     nodo_t *aux;
-    
-    if ( !fila_vazia(fila) ){
-        *dado = fila->cabeca->dado;
+    if (!(aux = malloc(sizeof(nodo_t))))
+        return 0;
 
+    *dado = fila->cabeca->dado;
+
+    if (fila_vazia(fila))
+    {
         aux = fila->cabeca->prox;
-        free (fila->cabeca);
         fila->cabeca = aux;
+        free(fila->cabeca);
         fila->tamanho--;
 
         return 1;
+    }
+    else
+    {
+
     }
 
     return 0;
 }
 
-int fila_tamanho (fila_t *fila)
+int fila_tamanho(fila_t *fila)
 {
     return fila->tamanho;
 }
 
-int fila_vazia (fila_t *fila)
+int fila_vazia(fila_t *fila)
 {
-    if ( (!fila->cabeca) && (!fila->cauda) )
+    if ((!fila->cabeca))
         return 1;
     return 0;
 }

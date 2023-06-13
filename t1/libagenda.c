02b01cc9c3327e742f90185b5058441d92642914
thifigma <thiago.figueiredo@ufpr.br>
2023-06-12 21:41:27 -0300

    Agenda

--- t1/libagenda.c
+++ t1/libagenda.c
@@ -1,3 +1,34 @@
 #include <stdlib.h>
 #include "libagenda.h"
 
+agenda_t* cria_agenda()
+{
+    agenda_t *ag;
+
+    if (!(ag = malloc (sizeof(agenda_t)))){
+        printf ("Erro de alocacao! \n");
+        return NULL;
+    }
+
+    ag->mes_atual = 1;
+    ag->ptr_mes_atual = 6;
+
+    return ag;
+}
+
+compromisso_t* cria_compromisso (horario_compromisso_t hc, int id,  char* descricao)
+{
+    compromisso_t *compr;
+
+    if (!(compr = malloc (sizeof(compromisso_t)))){
+        printf ("Erro de alocacao! \n");
+        return NULL;
+    }
+
+    compr->descricao = descricao;
+    compr->id = id;
+    compr->inicio = (hc.ini_h + hc.ini_m);
+    compr->fim = (hc.fim_h + hc.fim_m);
+
+    return compr;
+}

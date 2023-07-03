8d578ec7cf4211d3c6e93a492dedf45ef6636abf
Thiago <thiago.figueiredo@ufpr.br>
2023-07-03 17:18:16 -0300

    Testes

--- Tgrande/t1/agenda.c
+++ Tgrande/t1/agenda.c
@@ -4,6 +4,9 @@
 
 /*Aqui vai ser implementado o teste - 'firma' */
 
+#define REUNIOES 100
+
+
 typedef struct funcionario {
     int lideranca;
     int exp;
@@ -20,6 +23,16 @@ int aleat(int min, int max)
     return (rand() %max);
 }
 
+int aleat_mult_15(int min, int max)
+{
+    int num = rand() %max;
+
+    while (num % 15 != 0)
+        num = rand() %max;
+    
+    return num;
+}
+
 void inicia_colaborador(funcionario_t *colaborador)
 {
     for(int i = 0; i < 30; i++){
@@ -29,13 +42,41 @@ void inicia_colaborador(funcionario_t *colaborador)
     }
 }
 
-void inicia_terafas (tarefa_t *tarefas)
+void inicia_tarefas (tarefa_t *tarefas)
 {
     for (int i = 0; i < 100; i++){
         tarefas[i].tempo_conclusao = aleat (0, 100);
         tarefas[i].dificuldade = aleat (30, 80); 
     }
 }
+
+/*Verificar a questao dos ponteiros*/
+void inicia_compromisso (compromisso_t **reuniao, int *dia)
+{
+    horario_compromisso_t hc = inicia_hc (&hc);
+    *dia = aleat(1, 31);
+    int id = (0, REUNIOES - 1); /*Ajustar segundo parametro*/
+    char descricao; /*Ajustar descrcao conforme FIRMA e ajustar problemas de seguranca*/
+
+    (*reuniao) = cria_compromisso (hc, id, descricao);
+}
+
+compromisso_t *compromisso_atual (agenda_t *agenda_funcionario)
+{  
+    return agenda_funcionario->ptr_mes_atual->dias->comprs; 
+}
+
+horario_compromisso_t inicia_hc(horario_compromisso_t *hc)
+{
+    hc->ini_h = aleat(8, 12);
+    hc->ini_m = aleat_mult_15 (0, 45);
+    hc->fim_h = hc->ini_h + aleat(1, 4);
+    hc->fim_m = hc->ini_m;
+
+    return *hc;
+}
+
+
 
 int escolhe_lider (funcionario_t *colaborador)
 {
@@ -48,20 +89,66 @@ int escolhe_lider (funcionario_t *colaborador)
     return lider;
 }
 
+int escolhe_membro (funcionario_t *colaborador)
+{
+    int membro = aleat(0, 29);
+    
+    return membro;
+}
+
 int main () 
 {
     funcionario_t colaborador[30];
     tarefa_t tarefas[100];
-    mes_t meses;
+    compromisso_t *reuniao;
+
+    /*Indice do vetor colaborador*/
     int lider;
+    int dia;
+    int mes;
+    int membro;
 
-    inicia_colaborador(colaborador);
-    inicia_tarefas (tarefas);
 
-    for (meses.mes = 1; meses.mes <= 12; meses.mes++){
-        for (int i = 0; i < 100; i++){
-            lider = escolhe_lider(colaborador);
+    inicia_colaborador(&colaborador);
+    inicia_tarefas (&tarefas);
 
+    for (mes = 1; mes <= 12; mes++)
+        for (int i = 0; i < REUNIOES; i++){
+
+            lider = escolhe_lider(&colaborador);
+            
+            inicia_compromisso (&reuniao, &dia);
+            
+            if (marca_compromisso_agenda(colaborador[lider].ag, dia, reuniao) > 0){
+                  int max_membros = aleat (2, 6);
+
+                  for (int j = 0; j < max_membros; j++){
+                    membro = escolhe_membro(&colaborador);
+
+                    if (colaborador[lider].lideranca > colaborador[membro].lideranca + aleat(-20, 10))
+                        if (marca_compromisso_agenda(colaborador[membro].ag, dia, reuniao) < 0)
+                            desmarca_compromisso_agenda(colaborador[lider].ag, dia, reuniao);
+                    else
+                         desmarca_compromisso_agenda(colaborador[lider].ag, dia, reuniao);
+                }
+            }
+        }
+
+    for (mes = 1; mes < 12; mes ++){
+        for (dia = 1; dia < 31; dia ++){
+            for (int i = 0; i < REUNIOES; i++){
+                for (int id = 0; id < 30; id++){
+                    compromisso_t *compromisso = compromisso_atual(colaborador[id].ag);
+
+                    while (compromisso){
+                        int tarefa_id = compromisso->id;
+
+                        if(tarefas[tarefa_id].tempo_conclusao > 0){
+                            
+                        }
+                    }
+                }
+            }
         }
     }
     return 0;
